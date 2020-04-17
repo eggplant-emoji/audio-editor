a97f5b1f3ad449d59a97df8892ec2cff68a621b1
furetur <furetur@gmail.com>
2020-04-18 01:54:50 +0500

    added tests

---
+++ test_io.py
@@ -0,0 +1,15 @@
+import unittest
+import wave
+from audiodata import AudioData
+
+test_file_input = wave.open('test_input.wav', 'rb')
+test_file_output = wave.open('test_output.wav', 'wb')
+
+class TestReadWrite(unittest.TestCase):   
+    def test_reads_writes_without_errors(self):
+        r = AudioData.read(test_file_input)
+        r.write(test_file_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
