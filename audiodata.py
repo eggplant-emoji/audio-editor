2fda5122737ddbc4305341b9b1493fdcfe662357
furetur <furetur@gmail.com>
2020-04-18 00:59:13 +0500

    added documentation

--- audiodata.py
+++ audiodata.py
@@ -14,6 +14,14 @@ def join_framesdata(framesdata: List[bytes]) -> bytes:
 
 
 class AudioData:
+    """
+    Represents an audiofile, all the data it contains.
+    channels_number -- number of channels in the audiofile
+    framerate -- number of samples per second
+    frames_number -- total number of samples
+    compression_type -- only 'NONE' is supported
+    compression_name -- only 'not compressed' is supported
+    """
     channels_number: int
     sample_width: int
     framerate: int
@@ -24,11 +32,28 @@ class AudioData:
     framesdata: List[bytes]
 
     def __init__(self, params: tuple, framesdata: List[bytes]):
+        """
+        Creates new audiodata object
+        params are the parameters returned by Wave_read.getparams()
+        framesdata is a list of bytes where each byte represents a sample in an audiofile
+        """
         super().__init__()
         self.channels_number, self.sample_width, self.framerate, self.frames_number, self.compression_type, self.compression_name = params
         self.framesdata = framesdata
 
     def get_params(self, **overrides):
+        """
+        Returns parameters similarly to Wave_read.getparams() if overrides dict is empty
+        Otherwise if a named parameter is received it overrides the parameter in the returned tuple
+        Each keyword argument (key: value) overrides a returned parameter with name = key
+        Keyword arguments:
+          channels_number
+          sample_width
+          framerate
+          frames_number
+          compression_type
+          compression_name
+        """
         return (
             self.channels_number if 'channels_number' not in overrides else overrides['channels_number'],
             self.sample_width if 'sample_width' not in overrides else overrides['sample_width'],
@@ -39,22 +64,30 @@ class AudioData:
         )
 
     def reverse(self):
+        """
+        Reverses an audiodata.
+        Returns the audiodata that if played back sounds like the original
+        """
         new_frames = self.framesdata[::-1]
         return AudioData(self.get_params(), new_frames)
 
-    def join(self):
-        """ Method for compound two audiofiles
-        Accepts second file which will be join with first file
-        Returns AudioData"""
+    def join(self, audiodata):
+        """
+        Joins two audiodatas
+        Accepts the second audiodata
+        Returns a new audiodata that is essentially
+        two audiodatas joined together like self + audiodata
+        """
         new_framesdata = self.framesdata + audiodata.framesdata
         new_frames_number = self.frames_number + audiodata.frames_number
         new_params = self.get_params(frames_number=new_frames_number)
         return AudioData(new_params, new_framesdata)
 
     def crop(self, start_milis: int, end_milis: int):
-        """ Method for cropping an audiofile
-        Accepts audiodata and start_milis, end_milis
-        Creates a copy of audiodata that starts at [start_milis] miliseconds and ends at [end_milis] miliseconds
+        """
+        Croppes the audiodata
+        Returns a copy of audiodata that starts at start_milis miliseconds and ends at end_milis miliseconds
+        relative to the original audiodata
         """
         start_frame_index = self.frame_index_from_milis(start_milis)
         end_frame_index = self.frame_index_from_milis(end_milis)
@@ -67,11 +100,18 @@ class AudioData:
         return (self.framerate * milis) // 1000
 
     def write(self, file: wave.Wave_write) -> None:
+        """
+        Writes audio data to the file
+        """
         file.setparams(self.get_params())
         file.writeframes(join_framesdata(self.framesdata))
 
     @staticmethod
     def read(file: wave.Wave_read):
+        """
+        Reads file and produces an audiodata from its data
+        Returns that audiodata
+        """
         params = file.getparams()
         frames_number = file.getnframes()
         frames = file.readframes(frames_number)
@@ -80,19 +120,21 @@ class AudioData:
         return AudioData(params, framesdata)
 
     def speed_up(self, coefficient: int):
-        """ Method for speed-up an audiofile
-        Accepts audiofile and coefficient (how many times you need to speed-up the track)
-
-        Returns new audiofile """
+        """
+        Speeds up audiodata by a n times
+        Accepts a whole number n
+        Returns new audiodata
+        """
         new_framerate = self.framerate * coefficient
         new_params = self.get_params(framerate=new_framerate)
         return AudioData(new_params, self.framesdata)
 
     def slow_down(self, coefficient: int):
-        """ Method for slow down an audiofile
-         Accepts audiofile and coefficient (how many times you need to slow down the track)
-         Returns new audiodata"""
-
+        """
+        Slows down audiodata by n times
+        Accepts a whole number n
+        Returns a new slowed down audiodata
+        """
         new_framerate = self.framerate / coefficient
         new_params = self.get_params(framerate=new_framerate)
         return AudioData(new_params, self.framesdata)
