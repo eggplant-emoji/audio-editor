2694b90fa9b659942c52c163e0384f76ea69a430
annakarpova <annakarpova200@gmail.com>
2020-04-18 14:22:58 +0500

    refactor main

--- main.py
+++ main.py
@@ -3,28 +3,28 @@ from audiodata import AudioData
 keyboard_input = input()
 input_data = keyboard_input.split(' ')
 
-a = wave.open('input_data[1]', 'rb')
-d = AudioData.read(a)
+a = wave.open(input_data[1], 'rb')
+audiodata = AudioData.read(a)
 if input_data[0] == 'speed_up':
     coefficient = int(input_data[2])
-    d.speed_up(coefficient)
+    audiodata.speed_up(coefficient)
 
 elif input_data[0] == 'slow_down':
     coefficient = int(input_data[2])
-    d.speed_up(coefficient)
+    audiodata.speed_up(coefficient)
 
 elif input_data[0] == 'join':
-    b = wave.open('input_data[2]', 'rb')
-    c = AudioData.read(b)
-    c.join()
+    b = wave.open(input_data[2], 'rb')
+    audiodata2 = AudioData.read(b)
+    audiodata.join(audiodata2)
 
 elif input_data[0] == 'reserve':
-    d.reverse()
+    audiodata.reverse()
 
 elif input_data[0] == 'crop':
-    start_milis = input_data[2]
-    end_milis = input_data[3]
-    d.crop(start_milis, end_milis)
+    start_milis = int(input_data[2])
+    end_milis = int(input_data[3])
+    audiodata.crop(start_milis, end_milis)
 
 else:
     print('Not found')
